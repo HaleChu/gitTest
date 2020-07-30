e4aae17461e3bbdf858147c09816e09e4efb3aa8
GodChu <GodChu@example.com>
2020-07-30 17:50:02 +0800

    test1

---
+++ test1/src/main/java/pers/god/demo/web/PersonController.java
@@ -0,0 +1,26 @@
+package pers.god.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pers.god.demo.domain.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("person")
+public class PersonController {
+
+    @GetMapping("personList")
+    public List<Person> personList() {
+        Person p1 = new Person("张三", 20, "男");
+        Person p2 = new Person("李四", 19, "女");
+        Person p3 = new Person("王五", 21, "男");
+        List<Person> personList = new ArrayList<>();
+        personList.add(p1);
+        personList.add(p2);
+        personList.add(p3);
+        return personList;
+    }
+}
